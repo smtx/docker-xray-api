6bad63739be64326da7e12f95af4dad43e56a48d
Sebastian Rios <seba.rios@gmail.com>
2016-03-19 19:07:52 -0300

    allow to set charset

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,34 @@
 FROM node:latest
 MAINTAINER @smtx
 
-# Updating ubuntu packages
-RUN apt-get update
+# # Updating ubuntu packages
+# RUN apt-get update
 
-# Installing the packages needed to run Nightmare
-RUN apt-get install -y \
-  xvfb \
-  x11-xkb-utils \
-  xfonts-100dpi \
-  xfonts-75dpi \
-  xfonts-scalable \
-  xfonts-cyrillic \
-  x11-apps \
-  clang \
-  libdbus-1-dev \
-  libgtk2.0-dev \
-  libnotify-dev \
-  libgnome-keyring-dev \
-  libgconf2-dev \
-  libasound2-dev \
-  libcap-dev \
-  libcups2-dev \
-  libxtst-dev \
-  libxss1 \
-  libnss3-dev \
-  gcc-multilib \
-  g++-multilib
+# # Installing the packages needed to run Nightmare
+# RUN apt-get install -y \
+#   xvfb \
+#   x11-xkb-utils \
+#   xfonts-100dpi \
+#   xfonts-75dpi \
+#   xfonts-scalable \
+#   xfonts-cyrillic \
+#   x11-apps \
+#   clang \
+#   libdbus-1-dev \
+#   libgtk2.0-dev \
+#   libnotify-dev \
+#   libgnome-keyring-dev \
+#   libgconf2-dev \
+#   libasound2-dev \
+#   libcap-dev \
+#   libcups2-dev \
+#   libxtst-dev \
+#   libxss1 \
+#   libnss3-dev \
+#   gcc-multilib \
+#   g++-multilib
 
-ENV DEBUG="nightmare"
+# ENV DEBUG="nightmare"
 
 WORKDIR /home/api
 ADD . /home/api
@@ -37,4 +37,5 @@ RUN npm install
 
 EXPOSE 8888
 
-CMD xvfb-run --server-args="-screen 0 1024x768x24" node app.js
+# CMD xvfb-run --server-args="-screen 0 1024x768x24" node app.js
+CMD node app.js
--- app.js
+++ app.js
@@ -13,6 +13,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+
+var vo = require('vo');
+var url;
+function *process(){
+    // TODO: try catch error handling
+    var Nightmare = require('nightmare');
+    var nightmare = Nightmare({show:true});
+    var d = yield nightmare.goto(url)
+        .evaluate(function(){
+            return document.querySelectorAll('*')[0].innerHTML;
+        });
+    yield nightmare.end();
+    return d;
+}
+
 var router = express.Router();
 
 app.use(function(req, res, next) {
@@ -26,23 +41,11 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function (req, res) {
-  
   // get data with nightmare if we need to scrape ajax content.
   if (req.body.wait){
-      var vo = require('vo');
-      function *process(){
-        // TODO: try catch error handling
-        var Nightmare = require('nightmare');
-        var nightmare = Nightmare({show:true});
-        var d = yield nightmare.goto(req.body.url)
-            .evaluate(function(){
-                return document.querySelectorAll('*')[0].innerHTML;
-            });
-        yield nightmare.end();
-        return d;
-      }
       vo(process)(function(err,result){
           //TODO: error handling
+          url = req.body.goto;
           process_data(result);
       });
   } else {
@@ -51,63 +54,72 @@ router.post('/', function (req, res) {
 
 
   function process_data(d){
-    var Xray = require('smtx-ray');
-    var x = Xray();
-
-    // if xml flag exists, switch cheerio to admit XML special elements CDATA, etc.
-    if (req.body.xml){
-        var cheerio = require('cheerio');
-        cheerio.prototype.options.xmlMode = true;
-    }
-
-    var js = req.body.recipe;
-
-    if (req.body.paginate!==undefined && req.body.selector!==undefined){
-        if (req.body.limit!==undefined){
-        var limit = req.body.limit;
+    if (d != undefined){
+        var Xray = require('x-ray');
+        if (req.body.charset){
+            var request = require('./x-ray-driver');
+            var x = Xray().driver(request(req.body.charset));            
         } else {
-        var limit = 3;
+            var x = Xray();            
         }
-        var next = false;
-        var data = false;
-        var i = x(d, 'body', [req.body.paginate])
-                .paginate(req.body.paginate).limit(parseInt(limit))(function(err, obj) {
-                    if (err) return err;
-                    if ( parseInt(obj.length) < parseInt(limit) ){
-                    next = false;
-                    } else {
-                    next = obj.last();
-                    }
-                    if ( data ){
-                    res.json({data:data,next:next});
-                    }
-        });
-        var j = x(d, req.body.selector, [js])
-                .paginate(req.body.paginate).limit(parseInt(limit))(function(err, obj) {
-                    if ( next ){
-                    res.json({data:obj,next:next});
-                    } else {
-                    data = obj;
-                    }
-        });
 
-    } else {
-        // Normalize url
-        if (d.substring(0,2)=='//') d = 'http:'+d;
-        var j = x(d, js)(function(err, obj) {
-            if (req.body.regex){
-            if (typeof obj == 'string'){
-                obj = setRegex(obj,req.body.regex);
+        // if xml flag exists, switch cheerio to admit XML special elements CDATA, etc.
+        if (req.body.xml){
+            var cheerio = require('cheerio');
+            cheerio.prototype.options.xmlMode = true;
+        }
+
+        var js = req.body.recipe;
+
+        if (req.body.paginate!==undefined && req.body.selector!==undefined){
+            if (req.body.limit!==undefined){
+            var limit = req.body.limit;
             } else {
-                if (obj){
-                    Object.keys(obj).forEach(function(k){
-                        obj[k] = setRegex(obj[k],req.body.regex[k]);
-                    });
+            var limit = 3;
+            }
+            var next = false;
+            var data = false;
+            var i = x(d, 'body', [req.body.paginate])
+                    .paginate(req.body.paginate).limit(parseInt(limit))(function(err, obj) {
+                        if (err) return err;
+                        if ( parseInt(obj.length) < parseInt(limit) ){
+                        next = false;
+                        } else {
+                        next = obj.last();
+                        }
+                        if ( data ){
+                        res.json({data:data,next:next});
+                        }
+            });
+            var j = x(d, req.body.selector, [js])
+                    .paginate(req.body.paginate).limit(parseInt(limit))(function(err, obj) {
+                        if ( next ){
+                        res.json({data:obj,next:next});
+                        } else {
+                        data = obj;
+                        }
+            });
+
+        } else {
+            // Normalize url
+            if (d.substring(0,2)=='//') d = 'http:'+d;
+            var j = x(d, js)(function(err, obj) {
+                if (req.body.regex){
+                if (typeof obj == 'string'){
+                    obj = setRegex(obj,req.body.regex);
+                } else {
+                    if (obj){
+                        Object.keys(obj).forEach(function(k){
+                            obj[k] = setRegex(obj[k],req.body.regex[k]);
+                        });
+                    }
                 }
-            }
-            }
-            res.json(obj);
-        });
+                }
+                res.json(obj);
+            });
+        }        
+    } else {
+        res.send('error');
     }      
   }
 
